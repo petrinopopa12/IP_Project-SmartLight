da6449710455b2b661f995702e07795aa7a76c5b
Andrei Mihailescu <mihailescuandrei01@gmail.com>
2022-06-04 20:36:10 +0300

    Added main

--- main.py
+++ main.py
@@ -1,8 +1,89 @@
-# This is a sample Python script.
+import eventlet
+import json
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+from threading import Thread
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+
+from users import users
+from weather import weather
+from status import get_status
+from music import music
+from light import light
+from auth import bp
+
+eventlet.monkey_patch()
 
 
-# if __name__ == '__main__':
+app = None
+mqtt = None
+socketio = None
+thread = None
 
+
+def create_app():
+    global app
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'PDS19UkUAd1H1NiAqJjGAFT6KrN78W5J'
+
+    app.register_blueprint(bp)
+    app.register_blueprint(users)
+    app.register_blueprint(light)
+    app.register_blueprint(music)
+    app.register_blueprint(weather)
+
+    @app.route('/')
+    def welcome():
+        global thread
+        if thread is None:
+            thread = Thread(target=background_thread)
+            thread.daemon = True
+            thread.start()
+        return 'Welcome to Hue!'
+
+    return app
+
+
+def create_mqtt_app():
+    # Setup connection to mqtt broker
+    app.config['MQTT_BROKER_URL'] = '127.0.0.1'
+    app.config['MQTT_BROKER_PORT'] = 1883
+    app.config['MQTT_KEEPALIVE'] = 5
+    app.config['MQTT_TLS_ENABLED'] = False
+
+    global mqtt
+    mqtt = Mqtt(app)
+    global socketio 
+    socketio = SocketIO(app, async_mode="eventlet")
+
+    return mqtt
+
+# Start MQTT publishing
+# Function that every second publishes a message
+def background_thread():
+    count = 0
+    while True:
+        time.sleep(1)
+        # Using app context is required because the get_status() functions
+        # requires access to the db.
+        with app.app_context():
+            message = json.dumps(get_status(), default=str)
+        # Publish
+        mqtt.publish('python/mqtt', message)
+
+# App will now have to be run with `python app.py` as flask is now wrapped in socketio.
+# The following makes sure that socketio is also used
+
+def run_socketio_app():
+    create_app()
+    print(3)
+    create_mqtt_app()
+    print(4)
+    socketio.run(app, host='127.0.0.1', port=5000, use_reloader=False, debug=True)
+
+if __name__ == '__main__':
+    print(1)
+    run_socketio_app()
+    print(2)
